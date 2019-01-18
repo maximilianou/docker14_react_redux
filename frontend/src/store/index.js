875a450f1afbb82049af855ed7f79fd86e288755
Maximiliano Usich <maximilianou@gmail.com>
2019-01-18 13:36:41 -0300

    cleaning the docker compose name

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,4 @@
-// store/index.js
-import { createStore } from "redux";
-import rootReducer from "../reducers/index";
+import { createStore } from 'redux'; // store/index.js
+import rootReducer from '../reducers/index';
 const store = createStore(rootReducer);
 export default store;
