fbc2b6c7b6513bee7618f57a9faab6aa7405aeda
maximilianou <maximilianou@gmail.com>
2019-01-18 10:46:48 -0300

    react running jsx, redux store started clean

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,28 @@
+// REFERENCE: https://dev.to/valentinogagliardi/react-redux-tutorial-for-beginners-learning-redux-in-2018-13hj
+// App.js
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// + More than one class in the same file.
+// + Every class can be exported in ONE object as an attribute.
+//import React, { Component } from "react";
+class ExampleComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [
+        { title: "React Redux Tutorial for Beginners", id: 1 },
+        { title: "Redux e React: cos'è Redux e come usarlo con React", id: 2 }
+      ]
+    };
+  }
+  render() {
+    const { articles } = this.state;
+    return <ul>{articles.map(el => <li key={el.id}>{el.title}</li>)}</ul>;
+  }
+}
+
 
 class App extends Component {
   render() {
@@ -11,14 +33,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> Running!!.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <ExampleComponent/>
         </header>
       </div>
     );
